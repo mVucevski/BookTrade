6f8704937224aeac8ed3b15ca40b0151829541b1
Andrej Velevski <andrej_velevski@hotmail.com>
2018-07-04 09:39:48 +0200

    Added book class

---
+++ IT_BookTrade_1/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IT_BookTrade_1.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string ImageURL { get; set; }
+
+        [Range(1,10)]
+        public int Rating { get; set; }
+    }
+}
