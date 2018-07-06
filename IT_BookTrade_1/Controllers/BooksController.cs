4333076b81b7e32a4fb9dbfc6b87f2150f75a024
Andrej Velevski <andrej_velevski@hotmail.com>
2018-07-06 13:02:57 +0200

    Refactoring and Polish

--- IT_BookTrade_1/Controllers/BooksController.cs
+++ IT_BookTrade_1/Controllers/BooksController.cs
@@ -78,7 +78,7 @@ namespace IT_BookTrade_1.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Name,ImageURL,Rating")] Book book)
+        public ActionResult Edit([Bind(Include = "ID,Name,ImageURL,Rating,BookAuthor,BookDescription,Description,Price,Tradeable")] Book book)
         {
             if (ModelState.IsValid)
             {
